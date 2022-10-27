c8aa6b13e3fdd8acd59ccb7d4ad57d31be2b0caf
CheropS <sh.cherop@gmail.com>
2022-10-27 10:11:49 +0000

    adding folder for unittesting

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import os
+import unittest
+import models
+from datetime import datetime
+from models.base_model import BaseModel
+
+if __name__ = "__main__":
+    unittest.main()
